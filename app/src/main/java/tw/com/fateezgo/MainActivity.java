34a5c423d8b8062c4142798f47edafe6e2104ac5
fateezgo <fateezgo@gmail.com>
2016-08-23 15:13:45 +0800

    update By Josh 201608230313

--- app/src/main/java/tw/com/fateezgo/MainActivity.java
+++ app/src/main/java/tw/com/fateezgo/MainActivity.java
@@ -1,13 +1,63 @@
 package tw.com.fateezgo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-//new
+
+    private Button btn01;
+    private Button btn02;
+    private Button btn03;
+    private Button btn04;
+
+    //new
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+        btn01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),FindMasterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(),FreeMasterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(getApplicationContext(),FateEveryDayActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ModifyMemberActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void findViews() {
+        btn01 = (Button) findViewById(R.id.btn01);
+        btn02 = (Button) findViewById(R.id.btn02);
+        btn03 = (Button) findViewById(R.id.btn03);
+        btn04 = (Button) findViewById(R.id.btn04);
     }
 }
