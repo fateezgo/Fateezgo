6d75dd8ab801c7a2c66403298cbf737ee7d2ed24
fateezgo <fateezgo@gmail.com>
2016-08-29 00:08:48 +0800

    20160828

--- app/src/main/java/tw/com/fateezgo/NotLoginActivity.java
+++ app/src/main/java/tw/com/fateezgo/NotLoginActivity.java
@@ -1,13 +1,39 @@
 package tw.com.fateezgo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class NotLoginActivity extends AppCompatActivity {
-
+public class NotLoginActivity extends AppCompatActivity implements View.OnClickListener
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        Button login = (Button) this.findViewById(R.id.loginbtn),regist = (Button) this.findViewById(R.id.registbtn);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_not_login);
+
+        login.setOnClickListener(this);
+        regist.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        Intent intent;
+
+        switch (v.getId())
+        {
+            case R.id.loginbtn:
+                intent = new Intent(NotLoginActivity.this,LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.registbtn:
+                intent = new Intent(NotLoginActivity.this,RegistActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
