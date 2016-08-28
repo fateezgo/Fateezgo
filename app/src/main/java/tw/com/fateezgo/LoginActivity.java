a603bfddf2ec8dca80d50434fa2bbba7941b3ad5
fateezgo <fateezgo@gmail.com>
2016-08-28 15:38:11 +0800

    20160828

--- app/src/main/java/tw/com/fateezgo/LoginActivity.java
+++ app/src/main/java/tw/com/fateezgo/LoginActivity.java
@@ -2,14 +2,39 @@ package tw.com.fateezgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class LoginActivity extends AppCompatActivity
+public class LoginActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(this);
+        setSupportActionBar(toolbar);
+
+       if(getSupportActionBar() != null)
+       {
+           getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+           getSupportActionBar().setHomeButtonEnabled(true);
+       }
+
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return true;
     }
 }
