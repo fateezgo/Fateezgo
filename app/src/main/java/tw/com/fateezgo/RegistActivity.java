c6ba5824639a045fcf5d058123377acf29091b4d
fateezgo <fateezgo@gmail.com>
2016-08-30 01:57:20 +0800

    20160828

--- app/src/main/java/tw/com/fateezgo/RegistActivity.java
+++ app/src/main/java/tw/com/fateezgo/RegistActivity.java
@@ -2,12 +2,70 @@ package tw.com.fateezgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class RegistActivity extends AppCompatActivity {
-
+public class RegistActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener, View.OnClickListener {
+    EditText firstname,lastname,email,phone,passwd,retypepw;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+
+        Button teacherreg = (Button) this.findViewById(R.id.teacherreg),memberreg = (Button) this.findViewById(R.id.memberrage);
+
+        Toolbar toolbar = (Toolbar) RegistActivity.this.findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(this);
+        setSupportActionBar(toolbar);
+
+        if(getSupportActionBar() != null)
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+        Spinner countrycode = (Spinner) this.findViewById(R.id.countrycode);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(RegistActivity.this,R.array.list,android.R.layout.simple_spinner_dropdown_item);
+        countrycode.setAdapter(adapter);
+
+        firstname = (EditText) this.findViewById(R.id.firstname);
+        lastname = (EditText) this.findViewById(R.id.lastname);
+        email = (EditText) this.findViewById(R.id.email);
+        phone = (EditText) this.findViewById(R.id.phone);
+        passwd = (EditText)this.findViewById(R.id.passwd);
+        retypepw = (EditText) this.findViewById(R.id.retypepw);
+
+        teacherreg.setOnClickListener(this);
+        memberreg.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.teacherreg:
+                break;
+            case R.id.memberrage:
+                break;
+        }
     }
 }
