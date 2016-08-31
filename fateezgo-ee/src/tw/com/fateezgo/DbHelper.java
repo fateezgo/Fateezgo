e4bb3277c6239eef7a88d75d9efa12a16e9acbb7
fateezgo <fateezgo@gmail.com>
2016-08-31 11:06:52 +0800

    add Java EE project for Servlet.

---
+++ fateezgo-ee/src/tw/com/fateezgo/DbHelper.java
@@ -0,0 +1,46 @@
+package tw.com.fateezgo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbHelper {
+	Connection conn = null;
+	DbHelper() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://m.snpy.org/a105t2?user=a105t2&password=a5t2#&useUnicode=true&characterEncoding=UTF-8");		
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	String query(String qStr) {
+		String str = "";
+		Statement s;
+		try {
+			s = conn.createStatement();
+			ResultSet rs = s.executeQuery(qStr);
+			int columnCount = rs.getMetaData().getColumnCount();
+			System.out.println("column count: " + columnCount);
+			while (rs.next()) {
+				for (int i = 0; i < columnCount; i++) {
+					str += rs.getString(i+1);
+					str += ",";
+				}
+				str += "\n";
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return str;
+	}
+}
+
---
+++ fateezgo-ee/src/tw/com/fateezgo/MemServlet.java
@@ -0,0 +1,44 @@
+package tw.com.fateezgo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tw.com.fateezgo.DbHelper;
+
+/**
+ * Servlet implementation class MemServlet
+ */
+@WebServlet("/mem")
+public class MemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DbHelper db = new DbHelper();
+		String s = "Select name, email from MemberData;";
+		response.getWriter().append(db.query(s));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
