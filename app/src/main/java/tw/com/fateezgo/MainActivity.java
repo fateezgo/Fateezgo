19378a1ea11cc8ca501b0e184e56d5368777175d
fateezgo <fateezgo@gmail.com>
2016-09-01 15:56:13 +0800

    updateByJosh09010400

--- app/src/main/java/tw/com/fateezgo/MainActivity.java
+++ app/src/main/java/tw/com/fateezgo/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends BasicActivity {
         btn04.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),NewListActivity.class);
+                Intent intent = new Intent(getApplicationContext(),MemberActivity.class);
                 startActivity(intent);
             }
         });
