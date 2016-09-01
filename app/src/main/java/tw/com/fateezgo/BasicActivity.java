2c8403222954f5640c2ea66832fc717f0f607740
fateezgo <fateezgo@gmail.com>
2016-09-01 16:06:11 +0800

    1. fix back problem. 2. fix background color.

--- app/src/main/java/tw/com/fateezgo/BasicActivity.java
+++ app/src/main/java/tw/com/fateezgo/BasicActivity.java
@@ -137,6 +137,6 @@ public class BasicActivity extends AppCompatActivity {
                 Log.d("BasicActivity", "free area");
                 break;
         }
-        return true;
+        return false;
     }
 }
