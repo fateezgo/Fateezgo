3d49e53ef0d880aa13261ba770c8b88d04ffeea9
fateezgo <fateezgo@gmail.com>
2016-09-08 16:21:30 +0800

    Modify.

--- app/src/main/java/tw/com/fateezgo/RegistActivity.java
+++ app/src/main/java/tw/com/fateezgo/RegistActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class RegistActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener, View.OnClickListener {
+public class RegistActivity extends BasicActivity {
     EditText firstname,lastname,email,phone,passwd,retypepw;
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,59 +19,30 @@ public class RegistActivity extends AppCompatActivity implements Toolbar.OnMenuI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
 
-        Button teacherreg = (Button) this.findViewById(R.id.teacherreg),memberreg = (Button) this.findViewById(R.id.memberrage);
-
-        Toolbar toolbar = (Toolbar) RegistActivity.this.findViewById(R.id.toolbar);
-        toolbar.setOnMenuItemClickListener(this);
-        setSupportActionBar(toolbar);
-
-        if(getSupportActionBar() != null)
-        {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setHomeButtonEnabled(true);
-        }
-
-        Spinner countrycode = (Spinner) this.findViewById(R.id.countrycode);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(RegistActivity.this,R.array.list,android.R.layout.simple_spinner_dropdown_item);
-        countrycode.setAdapter(adapter);
-
         firstname = (EditText) this.findViewById(R.id.firstname);
-        lastname = (EditText) this.findViewById(R.id.lastname);
         email = (EditText) this.findViewById(R.id.email);
         phone = (EditText) this.findViewById(R.id.phone);
         passwd = (EditText)this.findViewById(R.id.passwd);
         retypepw = (EditText) this.findViewById(R.id.retypepw);
 
-        teacherreg.setOnClickListener(this);
-        memberreg.setOnClickListener(this);
     }
 
     @Override
-    public boolean onMenuItemClick(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return true;
+    void doViews() {
+        Intent intent = new Intent(this, MemberActivity.class);
+        startActivity(intent);
+        finish();
     }
 
-    @Override
-    public void onClick(View v)
-    {
-        Intent intent;
-        switch (v.getId())
-        {
-            case R.id.teacherreg:
-                intent = new Intent(RegistActivity.this,RegistMasterActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.memberrage:
-                intent = new Intent(RegistActivity.this,RegistMemberActivity.class);
-                startActivity(intent);
-                break;
-        }
+    void registAsMaster(View v) {
+        DbTask lt = new DbTask();
+        lt.execute("http://140.137.218.52:8080/fateezgo-ee/Register?type=master&name="+firstname.getText().toString() +
+        "&passwd=" + passwd.getText().toString() + "&phone=" + phone.getText().toString() + "&email=" + email.getText().toString());
+    }
+
+    void registAsMember(View v) {
+        DbTask lt = new DbTask();
+        lt.execute("http://140.137.218.52:8080/fateezgo-ee/Register?type=member&name="+firstname.getText().toString() +
+                "&passwd=" + passwd.getText().toString() + "&phone=" + phone.getText().toString() + "&email=" + email.getText().toString());
     }
 }
