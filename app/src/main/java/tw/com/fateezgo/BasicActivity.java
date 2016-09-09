4c37c7dba55dbb036f44029d81ca3855f6515474
fateezgo <fateezgo@gmail.com>
2016-09-09 11:33:47 +0800

    modifiy.

--- app/src/main/java/tw/com/fateezgo/BasicActivity.java
+++ app/src/main/java/tw/com/fateezgo/BasicActivity.java
@@ -95,7 +95,8 @@ abstract class BasicActivity extends AppCompatActivity {
         @Override
         protected ArrayList<String> doInBackground(String... params) {
             try {
-                URL url = new URL("http://140.137.218.52:8080/fateezgo-ee/" + params[0]);
+                URL url = new URL("http://j2.snpy.org:8080/fateezgo-ee/" + params[0]);
+                //URL url = new URL("http://140.137.218.52:8080/fateezgo-ee/" + params[0]);
                 InputStream is = url.openStream();
                 BufferedReader in = new BufferedReader(new InputStreamReader(is));
                 String strLine;
